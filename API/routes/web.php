4c31695a3b1520739cb48031c2a7dff8e145fdcc
iAntonAMC <1721110125@utectulancingo.edu.mx>
2023-05-30 22:53:21 -0600

    API POST is no working

--- API/routes/web.php
+++ API/routes/web.php
@@ -2,7 +2,6 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-
 // --- API ROUTES --- //
 
 # ROOT
@@ -89,10 +88,35 @@ $router->get('/docs/{doc_id}', function ($doc_id) {
 }
 );
 
+use Illuminate\Http\Request;
+# POST ONE DOCUMENT
+/* Saves one doc in db:
+    @return $response : JSON, HTTP code
+*/
+$router->post('/docs/new', function (Request $request) {
+    $doc_title = $request->input('doc_title');;
+    // $doc_body = "<p>Texto Guardado</p>";
+    try {
+        // Create the PDO instance.
+        $cnxn = new PDO("mysql:host=localhost; port=3306; dbname=se_docs", "root", "");
+        $cnxn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $cnxn -> exec("SET CHARACTER SET UTF8");
 
+        $query = "INSERT INTO documentos (TIPO_DOCTO, TITULO_DOCTO, CUERPO_DOCTO, PUBLICAR, CVE_NIVEL, CVE_CALENDARIO, CAMPUS, AUTOR_REGISTRO) VALUES
+        (2, $doc_title, '<p>Texto Guardado</p>', 'S', 0, 'Mayo - Agosto 2023', 1, '4');";
+        $cursor = $cnxn -> prepare($query);
+        $cursor -> execute();
 
-
-
+        return response()->json(['message' => 'Data Inserted in DB'], 200);
+    }
+    catch (Exception $E) {
+        return response()->json(['Error' => 'No se pudo insertar en la BD -> ' . $E], 400);
+    }
+    finally {
+        $cnxn = null;
+    }
+}
+);
 
 
 
