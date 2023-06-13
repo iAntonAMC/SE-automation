76b86e1a8cd5c883d8888de06545eb9d556e1965
iAntonAMC <1721110125@utectulancingo.edu.mx>
2023-06-13 12:44:44 -0600

    NEED to config editor 38.0.1(config.js)

--- FRONTEND/static/ckeditor5-38.0.1/config.js
+++ FRONTEND/static/ckeditor5-38.0.1/config.js
@@ -1,12 +1,27 @@
 DecoupledDocumentEditor
-    .create( document.querySelector( '#document-editor-container__editable' ) )
+    .create( document.querySelector( '#document-editor-container__editable' ), {
+        plugins: [ Pagination],
+        pagination: {
+            // A4
+            pageWidth: '21cm',
+            pageHeight: '29.7cm',
+
+            pageMargins: {
+                top: '20mm',
+                bottom: '20mm',
+                right: '12mm',
+                left: '12mm'
+            }
+        },
+        licenseKey: 'RkdrWENUZ2taU29FaUlvTWpaVVJVRnJpSFQ2U1YzY2E0dTFseWxCR0FTekdtRERNRmRjL2ZzN3lVZjQ9LU1qQXlNekEzTVRNPQ=='
+    })
     .then( editor => {
         window.editor = editor;
 
         const toolbarContainer = document.querySelector( '#document-editor__toolbar' );
 
         toolbarContainer.appendChild( editor.ui.view.toolbar.element );
-    } )
+    })
     .catch( error => {
         console.error( 'There was a problem loading the editor build' );
         console.error( error );
